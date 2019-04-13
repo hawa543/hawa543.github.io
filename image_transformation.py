74ea2b921450e84aeb87599c0f43c8f5acde5d33
Hawa D Borbordee <Hawazayzay9228858@gmail.com>
2019-04-13 16:07:11 -0400

    Updated

---
+++ image_transformation.py
@@ -0,0 +1,23 @@
+from  PIL import Image
+my_image=Image.open("hb.jpg")
+image_pixels=my_image.load()
+width,height= my_image.size
+for i in range(0,width):
+    for j in range(0,height):
+        current_pixel=image_pixels[i,j]
+        blue_component=image_pixels[i,j][0]
+        green_component=image_pixels[i,j][1]
+        red_component=image_pixels[i,j][2]
+        gray_value=(int)(0.07 * blue_component+0.72 * green_component + 0.21 * red_component)
+        if(gray_value<20):
+            new_color =(28,19,196,255)
+        elif(gray_value<40):
+            new_color=(196,19,175,255)
+        elif(gray_value<60):
+            new_color=(196,37,19,255)
+        elif(gray_value<100):
+            new_color=( 19,196,175,255)
+        else:
+            new_color=(119,196,19,255)
+        image_pixels[i,j]= new_color
+my_image.show()
